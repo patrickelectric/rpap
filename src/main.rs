5defdc50d3e9f72e0a3fe6f08ca368f510d981c5
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-12 14:13:41 -0300

    main: Add input
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,12 @@ fn main() {
                         .required(true),
                 )
                 .subcommand(
-                    SubCommand::with_name("OUTPUT")
+                    SubCommand::with_name("output")
                         .about("Set pin as output.")
                         .arg(
                             Arg::with_name("state")
                                 .help("Set state of pin.")
-                                .possible_values(&vec!["LOW", "HIGH"])
+                                .possible_values(&vec!["low", "high"])
                                 .required(true),
                         ),
                 )
@@ -38,31 +38,40 @@ fn main() {
         )
         .get_matches();
 
-    if matches.is_present("gpio") {
-        let gpio_matches = matches.subcommand_matches("gpio").unwrap();
-        let pin_number = gpio_matches
-            .value_of("PIN")
-            .unwrap()
-            .parse::<u8>()
-            .unwrap_or_else(|_| panic!("Expected PIN number between 0 and 27."));
-        let gpio = Gpio::new().unwrap().get(pin_number);
-        let gpio_unwraped = match gpio {
-            Result::Ok(val) => val,
-            Result::Err(err) => panic!("Error: {}", err),
-        };
-        let mut pin = gpio_unwraped.into_output();
-        pin.set_reset_on_drop(false);
-        if gpio_matches
-            .subcommand_matches("OUTPUT")
-            .unwrap()
-            .value_of("state")
-            .unwrap()
-            == "HIGH"
-        {
-            pin.set_high();
-        } else {
-            pin.set_low();
+    match matches.subcommand() {
+        ("gpio", Some(gpio_subcommand)) => {
+            let pin_number = gpio_subcommand
+                .value_of("PIN")
+                .unwrap()
+                .parse::<u8>()
+                .unwrap_or_else(|_| panic!("Expected PIN number between 0 and 27."));
+
+            let gpio = match Gpio::new().unwrap().get(pin_number) {
+                Result::Ok(val) => val,
+                Result::Err(err) => panic!("Error: {}", err),
+            };
+
+            match gpio_subcommand.subcommand() {
+                ("INPUT", Some(_)) => {
+                    let pin = gpio.into_input();
+                    println!("Pin {}:\t{}", pin.pin(), pin.read());
+                }
+
+                ("OUTPUT", Some(output_subcommand)) => {
+                    let mut pin = gpio.into_output();
+                    // Avoid reset state after finishing
+                    pin.set_reset_on_drop(false);
+
+                    let _ = match output_subcommand.value_of("state") {
+                        Some("high") => pin.set_high(),
+                        Some("low") => pin.set_low(),
+                        _ => unreachable!(),
+                    };
+                }
+
+                _ => unreachable!(),
+            };
         }
-        return;
-    }
+        _ => unreachable!(),
+    };
 }
